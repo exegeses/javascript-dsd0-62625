9ae9e363c213256b91d34abde66bc101798aeff0
Marcos <exegeses@gmail.com>
2022-09-14 14:56:18 -0300

    práctica funciones - pintarVerde()

---
+++ clase4/js/funciones.js
@@ -0,0 +1,16 @@
+//ubicamos elemento dentro del DOM
+const caja = document.querySelector('#caja');
+
+/* código sin ninguna interactividad 
+caja.style.backgroundColor = '#0d2';
+caja.innerText = 'verde';
+caja.style.color = '#000';
+*/
+
+//declaración 
+function pintarVerde()
+{
+    caja.style.backgroundColor = '#0d2';
+    caja.innerText = 'verde';
+    caja.style.color = '#000';
+}
